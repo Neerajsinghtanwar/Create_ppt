99f63542109dbbffd2221ec48106d8a1a8a98d1f
neeraj <neerajtanwar17@gmail.com>
2021-08-19 23:05:31 +0530

    commit

--- ppt_code.py
+++ ppt_code.py
@@ -1,4 +1,5 @@
 from pptx import Presentation
+from pptx.util import Cm, Inches, Pt
 from wand.image import Image
 import os
 
@@ -11,12 +12,20 @@ prs = Presentation()
 # choosing slide layout
 sl = prs.slide_layouts[8]
 
-# adding a slide
+# adding slides
 slide1 = prs.slides.add_slide(sl)
 slide2 = prs.slides.add_slide(sl)
 slide3 = prs.slides.add_slide(sl)
 slide4 = prs.slides.add_slide(sl)
 slide5 = prs.slides.add_slide(sl)
+
+# create directory for composite images
+try:
+    path = os.path.join("/home/neeraj/Desktop/Development/Python/Create_ppt/", "logo_images")
+    os.mkdir(path)
+
+except FileExistsError:
+    print("File is already exists")
 
 # image path
 logo = 'images/nike_black.png'
@@ -26,59 +35,40 @@ image3 = 'images/image3.jpg'
 image4 = 'images/image4.jpg'
 image5 = 'images/image5.jpg'
 
-# create directory for composite images
-path = os.path.join("/home/neeraj/Desktop/Development/Python/Create_ppt/", "logo_images")
-os.mkdir(path)
-
 # set logo in image
-with Image(filename=image1) as main_img:
-    main_img.resize(1000, 800)
-    with Image(filename=logo) as logo_img:
-        logo_img.resize(330, 110)
-        main_img.composite(logo_img, left=15, top=35)
-    main_img.save(filename="logo_images/logo_image1.jpg")
-
-with Image(filename=image2) as main_img:
-    main_img.resize(1000, 800)
-    with Image(filename=logo) as logo_img:
-        logo_img.resize(330, 110)
-        main_img.composite(logo_img, left=15, top=35)
-    main_img.save(filename="logo_images/logo_image2.jpg")
-
-with Image(filename=image3) as main_img:
-    main_img.resize(1000, 800)
-    with Image(filename=logo) as logo_img:
-        logo_img.resize(330, 110)
-        main_img.composite(logo_img, left=15, top=35)
-    main_img.save(filename="logo_images/logo_image3.jpg")
-
-with Image(filename=image4) as main_img:
-    main_img.resize(1000, 800)
-    with Image(filename=logo) as logo_img:
-        logo_img.resize(330, 110)
-        main_img.composite(logo_img, left=15, top=35)
-    main_img.save(filename="logo_images/logo_image4.jpg")
-
-with Image(filename=image5) as main_img:
-    main_img.resize(1000, 800)
-    with Image(filename=logo) as logo_img:
-        logo_img.resize(330, 110)
-        main_img.composite(logo_img, left=15, top=35)
-    main_img.save(filename="logo_images/logo_image5.jpg")
+images = [image1, image2, image3, image4, image5]
+x = 0
+for i in images:
+    x = x+1
+    print("set logo in Image%s is complete" % x)
+    with Image(filename=i) as img:
+        img.resize(500, 400)
+        with Image(filename=logo) as logo_img:
+            logo_img.resize(170, 60)
+            img.composite(logo_img, left=5, top=5)
+            loc = "logo_images/logo_image%s.jpg" % x
+        img.save(filename=loc)
 
 # adding Images
-slide1.placeholders[1].insert_picture("logo_images/logo_image1.jpg")
-slide2.placeholders[1].insert_picture("logo_images/logo_image2.jpg")
-slide3.placeholders[1].insert_picture("logo_images/logo_image3.jpg")
-slide4.placeholders[1].insert_picture("logo_images/logo_image4.jpg")
-slide5.placeholders[1].insert_picture("logo_images/logo_image5.jpg")
+slides = [slide1, slide2, slide3, slide4, slide5]
+x = 0
+for i in slides:
+    x = x + 1
+    title = i.shapes
+    top = Inches(1.5)
+    left = Inches(1.5)
+    title.add_picture("logo_images/logo_image%s.jpg" % x, left, top)
 
 # adding a title in slide
-slide1.shapes.title.text = "This is a title."
-slide2.shapes.title.text = "This is a title."
-slide3.shapes.title.text = "This is a title."
-slide4.shapes.title.text = "This is a title."
-slide5.shapes.title.text = "This is a title."
+for i in slides:
+    left = Inches(1.4)
+    top = Inches(0.5)
+    width = height = Inches(7)
+    txt = i.shapes.add_textbox(left, top, width, height)
+    tf = txt.text_frame
+    p = tf.add_paragraph()
+    p.text = "This ppt file is created by Python"
+    p.font.size = Pt(30)
 
 # saving file
 prs.save("my_ppt.ppt")
